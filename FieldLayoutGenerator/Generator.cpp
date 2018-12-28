368a63a708194a634e589ee557c07b860a83f5ed
Kuznetsov Stepan <stepan14511@gmail.com>
2018-12-28 16:32:35 +0300

    Added generation of java file(resources array). Deleted system C++ files.

--- FieldLayoutGenerator/Generator.cpp
+++ FieldLayoutGenerator/Generator.cpp
@@ -13,19 +13,38 @@ int main(){
     cout << "Size(one number)(in dp's): ";
     int size_of_block;
     cin >> size_of_block;
-    ofstream output_file;
-    output_file.open("OUTPUT_CODE.txt");
+    ofstream output_file_java;
+    ofstream output_file_xml;
+    output_file_java.open("OUTPUT_JAVA.txt");
+    output_file_xml.open("OUTPUT_XML.txt");
+    output_file_java << "{\n";
     for(int i = 0; i < lines; i++){
         for(int j = 0; j < arrows; j++){
-            output_file<<"<ImageView android:id=\"@+id/fld_"<<i<<"_"<<j<<"\" android:layout_width=\""<<size_of_block<<"dp\" android:layout_height=\""<<size_of_block<<"dp\" android:background=\"#FFFFFF\"";
+            //xml generation
+            output_file_xml<<"<ImageView android:id=\"@+id/fld_"<<i<<"_"<<j<<"\" android:layout_width=\""<<size_of_block<<"dp\" android:layout_height=\""<<size_of_block<<"dp\" android:background=\"#FFFFFF\"";
             if(i != 0)
-                output_file<<" android:layout_below=\"@id/fld_"<<i-1<<"_"<<j<<"\" android:layout_marginTop=\"2dp\"";
+                output_file_xml<<" android:layout_below=\"@id/fld_"<<i-1<<"_"<<j<<"\" android:layout_marginTop=\"2dp\"";
             if(j != 0)
-                output_file<<" android:layout_toRightOf=\"@id/fld_"<<i<<"_"<<j-1<<"\" android:layout_marginLeft=\"2dp\"";
-            output_file << "/>\n";
+                output_file_xml<<" android:layout_toRightOf=\"@id/fld_"<<i<<"_"<<j-1<<"\" android:layout_marginLeft=\"2dp\"";
+            output_file_xml << "/>\n";
+
+            //java generation
+            if(j == 0)
+                output_file_java << "{";
+            output_file_java << "R.id.fld_" << i << "_" << j;
+            if(j == arrows - 1){
+                if(i == lines - 1)
+                    output_file_java << "}};";
+                else
+                    output_file_java << "},";
+            }
+            else
+                output_file_java << ", ";
         }
-        output_file << "\n";
+        output_file_xml << "\n";
+        output_file_java << "\n";
     }
-    output_file.close();
+    output_file_xml.close();
+    output_file_java.close();
     return 0;
 }
--- FieldLayoutGenerator/README.txt
+++ FieldLayoutGenerator/README.txt
@@ -1,1 +1,5 @@
 I'm lazy, so i don't want to learn content providers yet(mb, it'll do it in future(MB, not sure)), so i created this C++ code for generating part of code, which is creating game field.
+
+
+P.s. OUTPUT_XML for .xml layout file
+ and OUTPUT_JAVA for Field.java file for RESOURCES array.
